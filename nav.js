f385e950b68fdf7eae4951003e1b71a4e803ba5f
Anar044 <51967882+Anar044@users.noreply.github.com>
2026-08-21 15:59:32 +0400

    Refactor navigation HTML structure in nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,21 @@
-document.write('<header><nav><a class="logo" href="index.html">anar <span>system</span></a><div class="links"><a href="solutions.html">Решения</a><a href="about.html">О нас</a><a href="customers.html">Клиенты</a><a href="support.html">Поддержка</a><a href="blog.html">Блог</a><a href="index.html#calculator">Тарифы</a></div></nav></header><script src="chat.js"><\/script>');
+document.write(`
+<header>
+    <nav>
+        <a class="logo" href="index.html">
+            anar <span>system</span>
+        </a>
+
+        <div class="links">
+            <a href="solutions.html">Решения</a>
+            <a href="about.html">О нас</a>
+            <a href="customers.html">Клиенты</a>
+            <a href="support.html">Поддержка</a>
+            <a href="blog.html">Блог</a>
+            <a href="reports.html">Отчёты</a>
+            <a href="index.html#calculator">Тарифы</a>
+        </div>
+    </nav>
+</header>
+
+<script src="chat.js"><\/script>
+`);
